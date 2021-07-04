72bb47a2e5d758ff7b08e4bf1f7bef617fa9223f
millerh1 <millerh1@livemail.uthscsa.edu>
2021-07-04 13:06:59 -0500

    working on API and models prt II

--- fibrodb/db.py
+++ fibrodb/db.py
@@ -9,3 +9,10 @@ def clean_init_db(db):
 
 def load_db(db):
     """Load all data into the db"""
+    # TODO: Need loading functions here instead of test data...
+    gene = Genes(
+        gene_id=1, gene_biotype="Coding", gene_symbol="TP53",
+        ensemble_id="ENSG0912039", seqnames="Chr1", start=1, end=10
+    )
+    db.session.add(gene)
+    db.session.commit()
